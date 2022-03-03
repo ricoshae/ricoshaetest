236f88f16665f6e6f6d6e8d3fa94fae8d59463eb
ricoshae <ricoshae@192-168-1-100.tpgi.com.au>
2022-03-03 13:46:50 +1000

    ud

---
+++ files/default.js
@@ -0,0 +1,7 @@
+< script >
+  if (document.referrer.match(/^https?:\/\/([^\/]+\.)?moodle311.ricoshae.com.au(\/|$)/i)) {
+
+  } else {
+    window.location.replace("/ricoshaetest/files/noaccess.html");
+  } <
+  /script>
